49fd686dc9a18f09a22c7ed783a7dd3f9ab7bd6b
Kate-IQA <81325024+Kate-IQA@users.noreply.github.com>
2021-05-22 17:14:52 +0300

    Update BonusService.java

--- src/BonusService.java
+++ src/BonusService.java
@@ -1,10 +1,11 @@
 public class BonusService {
-    public long calculate (long amount, int percent) {
-        amount = 100;
-        percent = 100;
-
-        long bonus = amount + percent;
-        return bonus;
+    public void calculate(long account, int invoice) {
+        int bonus = 0;
+        if (invoice > 1000) {
+            bonus = invoice / 100;
+        }
+        account = account + invoice + bonus;
+        System.out.println("Иготовая сумма на счете клиента = " + account);
+        System.out.println("Сумма бонусов на счете клиента = " + bonus);
     }
-
 }
