f71b4b2f9e753dfb287b580a0b35b96f6a48a733
Kate-IQA <81325024+Kate-IQA@users.noreply.github.com>
2021-05-22 17:18:38 +0300

    Update and rename MainV5.java to Solution.java

--- src/MainV5.java
+++
@@ -1,9 +0,0 @@
-public class MainV5 {
-    public static void main(String[] args) {
-        BonusService service = new BonusService();
-
-        long bonus = service.calculate (100,100);
-
-        System.out.println(bonus);
-    }
-}
---
+++ src/Solution.java
@@ -0,0 +1,8 @@
+public class Solution {
+    public static void main(String[] args) {
+
+        BonusService service = new BonusService();
+
+        service.calculate (100,1200);
+    }
+}
